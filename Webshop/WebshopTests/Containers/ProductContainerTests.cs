c89818ceacbce80a16b11c67c1a79ef463d85f92
Amit Dhiraj Sanchit <Dhirajsanchit@gmail.com>
2022-11-21 23:18:12 +0100

    Front end fixes

--- Webshop/WebshopTests/Containers/ProductContainerTests.cs
+++ Webshop/WebshopTests/Containers/ProductContainerTests.cs
@@ -1,6 +1,29 @@
+using BusinessLogicLayer.Classes;
+using BusinessLogicLayer.Containers;
+using InterfaceLayer.DALs;
+using WebshopTests.Mocks;
+
 namespace WebshopTests.Containers;
+
 
 public class ProductContainerTests
 {
+    static IProductDAL _productDal = new ProductDalMock();
+    ProductContainer productContainer = new ProductContainer(_productDal);
     
+    [Fact]
+    public void GetProductById_WhenProductExists_ReturnsProduct()
+    {
+        // Arrange
+      
+        
+        var product = new Product();
+        productContainer.AddProduct(product);
+
+        // Act
+        var result = productContainer.GetProductById(product.ProductId);
+
+        // Assert
+        Assert.Equal(product, result);
+    }
 }
